76a14d2ad5db447b03dd20a090df1daace306845
usaid7015 <49516724+usaid7015@users.noreply.github.com>
2022-07-25 00:14:39 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,5 +4,24 @@ toggle.addEventListener('click',() =>{
     document.body.classList.toggle('dark')
 });
 
-
+$('.product').owlCarousel({
+    loop:true,
+    margin:50,
+    nav: false,
+    autoplay: true,
+    stagePadding: 20,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:3,
+        },
+        1000:{
+            items:4,
+        }
+    }
+})
 
